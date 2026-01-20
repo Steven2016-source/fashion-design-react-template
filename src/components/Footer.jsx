a044588be5fb9d700a1b9a2dd386ceeb3c52dbc0
Stephen Aderibigbe <toluwalopestephena@gmail.com>
2026-01-20 08:33:12 +0100

    Gallery

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer(){
   return(
     <footer>
-      <p>Locacted @ Number 4 Ire-akari area, ogbomoso, Oyo State</p>
+      <p>Locacted @ Number 4 Ire-akari area, ogbomoso, Oyo State</p><br></br>
       <p>© 2026 PAPA STITCHES with Stephena | 09029348422</p>
     </footer>
   )
